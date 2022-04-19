94a6afccd04b58529f395a562c4036f849e629f0
SeanDuffie <sean.duffie@tamu.edu>
2022-04-18 22:42:51 -0500

    Clean Up

--- WiFiManager_Example.ino
+++ WiFiManager_Example.ino
@@ -213,7 +213,6 @@ void loop() {
     debugln("================================");
   }
   if (res) {wm.resetSettings();}
-//  rainbowCycle(1);
 }
   
 // Config Mode Function, Starts AP for configuring 
@@ -223,31 +222,3 @@ void configModeCallback(WiFiManager *myWiFiManager) {
 
   debugln(myWiFiManager->getConfigPortalSSID());
 }
-
-//// Slightly different, this makes the rainbow equally distributed throughout
-//void rainbowCycle(uint8_t wait) {
-//  uint16_t i, j;
-//
-//  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-//    for(i=0; i< strip.numPixels(); i++) {
-//      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-//    }
-//    strip.show();
-//    delay(wait);
-//  }
-//}
-//
-//// Input a value 0 to 255 to get a color value.
-//// The colours are a transition r - g - b - back to r.
-//uint32_t Wheel(byte WheelPos) {
-//  WheelPos = 255 - WheelPos;
-//  if(WheelPos < 85) {
-//    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-//  }
-//  if(WheelPos < 170) {
-//    WheelPos -= 85;
-//    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-//  }
-//  WheelPos -= 170;
-//  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-//}
