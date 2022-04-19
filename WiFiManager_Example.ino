2b372908ecfd2252ff0cc247a9da7fc3c264331f
SeanDuffie <sean.duffie@tamu.edu>
2022-04-18 22:41:45 -0500

    Add Debug Mode; Clean Up Pins; Remove LED Ring;

--- WiFiManager_Example.ino
+++ WiFiManager_Example.ino
@@ -4,14 +4,17 @@
  * If none are found and successfully connected, a wireless AP will be created for configuration
  */
 #include <WiFiManager.h>
-#include <ESP8266WiFi.h> // Used to get MAC
-#include <Adafruit_NeoPixel.h>
+#include <ESP8266WiFi.h> // Used to get MAC Address
 
-#define CNT 24
-#define PIN 1
+#define DEBUG 0
 
-// #define TRIGGER_PIN 1
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(CNT, PIN, NEO_GRB + NEO_KHZ800);
+#if DEBUG == 1
+#define debug(x) Serial.print(x)
+#define debugln(x) Serial.println(x)
+#else
+#define debug(x)
+#define debugln(x)
+#endif
 
 WiFiManager wm;
 
@@ -35,25 +38,13 @@ const int output2 = 2;
 const int output3 = 3;
 
 void setup() {
-//  WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP  
+//  WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
   Serial.begin(115200);
-//  Serial.setDebugOutput(true);
+  Serial.setDebugOutput(true);
 
-  // Initialize the output variables as outputs
-  pinMode(output0, OUTPUT);
-  pinMode(output1, OUTPUT);
-  pinMode(output2, OUTPUT);
-  pinMode(output3, OUTPUT);
-  // Set outputs to LOW
-  digitalWrite(output0, LOW);
-  digitalWrite(output1, LOW);
-  digitalWrite(output2, LOW);
-  digitalWrite(output3, LOW);
-
-  Serial.println("\n--------------------------------");
-  Serial.println("** Starting...");
-  
-  Serial.println("** Device MAC Address: " + WiFi.macAddress());
+  debugln("\n--------------------------------");
+  debugln("** Starting...");
+  debugln("** Device MAC Address: " + WiFi.macAddress());
 
   wm.setAPCallback(configModeCallback);     // Callback Function to Config Mode
 
@@ -64,34 +55,40 @@ void setup() {
   wm.setConfigPortalTimeout(120);           // How long to wait in Config Mode
   wm.setConnectTimeout(20);                 // How long to try to connect
   wm.setClass("invert");                    // Dark Theme
-  wm.setDebugOutput(false);                 // Comment out to enable Debug
+//  wm.setDebugOutput(false);                 // Comment out to enable Debug
   
   if (!wm.autoConnect("AutoConnectAP")) {   // Handles AutoConnect Failures
-    Serial.println("** Failed to connect or hit timeout");
-    Serial.println("** Restarting in 3 seconds...");
-    Serial.println("--------------------------------");
+    debugln("** Failed to connect or hit timeout");
+    debugln("** Restarting in 3 seconds...");
+    debugln("--------------------------------");
     delay(3000);
     ESP.restart();
   }
 
-  server.begin(); // Start the server
-  Serial.print("** Local IP: ");
-  Serial.println(WiFi.localIP());    // Print the IP address
-  Serial.println("** Setup Complete!");
-  Serial.println("--------------------------------");
+  // Initialize the output variables as outputs
+  pinMode(output0, OUTPUT);
+//  pinMode(output1, OUTPUT);
+  pinMode(output2, OUTPUT);
+  pinMode(output3, OUTPUT);
+  // Set outputs to LOW
+  digitalWrite(output0, LOW);
+//  digitalWrite(output1, LOW);
+  digitalWrite(output2, LOW);
+  digitalWrite(output3, LOW);
 
-  strip.begin();
-  strip.setBrightness(50);
-  strip.show(); // Initialize all pixels to 'off'
+  server.begin(); // Start the server
+  debug("** Local IP: ");
+  debugln(WiFi.localIP());    // Print the IP address
+  debugln("** Setup Complete!");
+  debugln("--------------------------------");
 }
 
 void loop() {
   // Check if a client has connected
   WiFiClient client = server.available();
-  client.setTimeout(5000);                // default is 1000
   if (client) {                           // Only runs if the client is connected
-    Serial.println("\n================================");
-    Serial.println("* New Client.");          // print a message out in the serial port
+    debugln("\n================================");
+    debugln("* New Client.");
     String currentLine = "";                // make a String to hold incoming data from the client
     while (client.connected()) {            // loop while the client's connected
       if (client.available()) {             // if there's bytes to read from the client,
@@ -111,39 +108,38 @@ void loop() {
             
             // turns the GPIOs on and off
             if (header.indexOf("GET /3/on") >= 0) {
-              Serial.println("* GPIO 3 on");
+              debugln("* GPIO 3 on");
               output3State = "on";
               digitalWrite(output3, HIGH);
             } else if (header.indexOf("GET /3/off") >= 0) {
-              Serial.println("* GPIO 3 off");
+              debugln("* GPIO 3 off");
               output3State = "off";
               digitalWrite(output3, LOW);
             } else if (header.indexOf("GET /2/on") >= 0) {
-              Serial.println("* GPIO 2 on");
+              debugln("* GPIO 2 on");
               output2State = "on";
               digitalWrite(output2, HIGH);
             } else if (header.indexOf("GET /2/off") >= 0) {
-              Serial.println("* GPIO 2 off");
+              debugln("* GPIO 2 off");
               output2State = "off";
               digitalWrite(output2, LOW);
-            } else if (header.indexOf("GET /1/on") >= 0) {
-              Serial.println("* GPIO 1 on");
-              output1State = "on";
-              digitalWrite(output1, HIGH);
-            } else if (header.indexOf("GET /1/off") >= 0) {
-              Serial.println("* GPIO 1 off");
-              output1State = "off";
-              digitalWrite(output1, LOW);
+//            } else if (header.indexOf("GET /1/on") >= 0) {
+//              debugln("* GPIO 1 on");
+//              output1State = "on";
+//              digitalWrite(output1, HIGH);
+//            } else if (header.indexOf("GET /1/off") >= 0) {
+//              debugln("* GPIO 1 off");
+//              output1State = "off";
+//              digitalWrite(output1, LOW);
             } else if (header.indexOf("GET /0/on") >= 0) {
-              Serial.println("* GPIO 0 on");
+              debugln("* GPIO 0 on");
               output0State = "on";
               digitalWrite(output0, HIGH);
             } else if (header.indexOf("GET /0/off") >= 0) {
-              Serial.println("* GPIO 0 off");
+              debugln("* GPIO 0 off");
               output0State = "off";
               digitalWrite(output0, LOW);
             }
-            
             
             
             // Display the HTML web page
@@ -178,14 +174,14 @@ void loop() {
               client.println("<p><a href=\"/2/off\"><button class=\"button button2\">OFF</button></a></p>");
             } 
             
-            // Display current state, and ON/OFF buttons for GPIO 1
-            client.println("<p>GPIO 1 - State " + output1State + "</p>");
-            // If the output1State is off, it displays the ON button       
-            if (output1State=="off") {
-              client.println("<p><a href=\"/1/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/1/off\"><button class=\"button button2\">OFF</button></a></p>");
-            } 
+//            // Display current state, and ON/OFF buttons for GPIO 1
+//            client.println("<p>GPIO 1 - State " + output1State + "</p>");
+//            // If the output1State is off, it displays the ON button       
+//            if (output1State=="off") {
+//              client.println("<p><a href=\"/1/on\"><button class=\"button\">ON</button></a></p>");
+//            } else {
+//              client.println("<p><a href=\"/1/off\"><button class=\"button button2\">OFF</button></a></p>");
+//            } 
             
             // Display current state, and ON/OFF buttons for GPIO 0
             client.println("<p>GPIO 0 - State " + output0State + "</p>");
@@ -213,45 +209,45 @@ void loop() {
     header = "";
     // Close the connection
     client.stop();
-    Serial.println("* Client disconnected.");
-    Serial.println("================================");
+    debugln("* Client disconnected.");
+    debugln("================================");
   }
   if (res) {wm.resetSettings();}
-  rainbowCycle(1);
+//  rainbowCycle(1);
 }
   
 // Config Mode Function, Starts AP for configuring 
 void configModeCallback(WiFiManager *myWiFiManager) {
-  Serial.println("Entered config mode");
-  Serial.println(WiFi.softAPIP());
+  debugln("Entered config mode");
+  debugln(WiFi.softAPIP());
 
-  Serial.println(myWiFiManager->getConfigPortalSSID());
+  debugln(myWiFiManager->getConfigPortalSSID());
 }
 
-// Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
-}
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  if(WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-}
+//// Slightly different, this makes the rainbow equally distributed throughout
+//void rainbowCycle(uint8_t wait) {
+//  uint16_t i, j;
+//
+//  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+//    for(i=0; i< strip.numPixels(); i++) {
+//      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+//    }
+//    strip.show();
+//    delay(wait);
+//  }
+//}
+//
+//// Input a value 0 to 255 to get a color value.
+//// The colours are a transition r - g - b - back to r.
+//uint32_t Wheel(byte WheelPos) {
+//  WheelPos = 255 - WheelPos;
+//  if(WheelPos < 85) {
+//    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+//  }
+//  if(WheelPos < 170) {
+//    WheelPos -= 85;
+//    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+//  }
+//  WheelPos -= 170;
+//  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+//}
